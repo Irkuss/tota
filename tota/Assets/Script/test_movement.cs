e747f939b0c234b12c0b683a6a1174317745885b
Tanguy Desgouttes <tanguy.desgouttes@epita.fr>
2019-01-27 21:33:39 +0100

    First Unity base movement

---
+++ tota/Assets/Script/test_movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_movement : MonoBehaviour {
+
+    public float speed;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(h, 0.0f, v);
+        if (h != 0f || v != 0f)
+            GetComponent<Rigidbody>().velocity = direction * speed * Time.deltaTime;
+    }
+}
