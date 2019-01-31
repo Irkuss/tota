05bdb20e0f51911ffb009f7cc4e4df1756d5cac6
yanis chaabane <yanis.chaabane@epita.fr>
2019-01-31 20:13:30 +0100

    Menu's scene

---
+++ tota/Assets/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Solo()
+    {
+        SceneManager.LoadScene(2); // METTRE L'INDEX DU SOLO OU "SOLO"
+    }
+
+    public void Multiplayer()
+    {
+        SceneManager.LoadScene(1); // METTRE L'INDEX DU MULTI OU "MULTIJOUEUR"
+    }
+
+    public void Credits()
+    {
+        // LE MIEUX EST DE FAIRE UN PANEL AVEC UNE ANIMATION
+    }
+
+    
+}
