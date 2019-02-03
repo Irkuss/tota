326a6da3062a7d5368af1679c21a6737059f38c2
Tanguy Desgouttes <tanguy.desgouttes@epita.fr>
2019-02-03 01:03:55 +0100

    Leave Prefab

---
+++ tota/Assets/Script/PlaygroundManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class PlaygroundManager : MonoBehaviour
+{
+    #region Photon Messages
+    
+    public void OnLeftRoom()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    #endregion
+
+
+    #region Public Methods
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    #endregion
+}
