dbef7cb0ea04a7a1e4f964ac24a4b42374520d5e
Jordan FAILLOUX <jordan.failloux@epita.fr>
2019-03-12 14:55:54 +0100

    Horloge

--- tota/Assets/Script/DayNight.cs
+++ tota/Assets/Script/DayNight.cs
@@ -6,30 +6,50 @@ public class DayNight : MonoBehaviour
     public float speed;
     public float heure;
     public float slider;
+    private bool changedHour;
+
+    public delegate void TimerHour();
+    public static event TimerHour onNewHour;
+
+    private void UpdateCallback()
+    {
+        if (onNewHour != null) //Si une personne nous écoute
+        {
+            onNewHour(); //Declenche le callback chez les spectateurs
+        }
+    }
 
     void Start()
     {
         sun = GetComponent<Light>();
         transform.rotation = Quaternion.Euler(0, -30, 0);
         slider = 0.5f;
+        
     }
-
-
+    
     void Update()
     {
-        if (slider >= 1.0)
+        if (slider >= 1.0)//Reset le slider à 0 pour faire un cycle
         {
             slider = 0;
+        }
+
+        if(changedHour)
+        {
+            UpdateCallback();
         }
 
         heure = slider * 24;
         sun.transform.rotation = Quaternion.Euler((slider * 360) - 90, 0, 0);
         slider = slider + Time.deltaTime / speed;
 
+        int newHeure = (int)(slider * 24); //Calcule l'heure suivante
+        changedHour = newHeure - (int)heure > 0; //Fais la différence pour voir si on a changé d'heure
+        
         float posx = transform.eulerAngles.x;
-        if (posx > -10 && posx < 40 || posx > 150 && posx < 190)
+        if (posx > -10 && posx < 40 || posx > 150 && posx < 190)//Change la couleur en orangé au levé et au couché du soleil
         {
-            sun.color = new Color(1f, 0.772549f, 0.4156863f, 1);
+            sun.color = new Color(1f, 0.772549f, 0.4156863f, 1f);
         }
         else
         {
