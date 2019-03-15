81174043d28ae862ea948ca1494395b843e61edc
Tanguy Desgouttes <tanguy.desgouttes@epita.fr>
2019-03-15 01:55:45 +0100

    added SO generation

---
+++ tota/Assets/Script/Scriptable Objects/Generation.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Generation")]
+public class Generation : ScriptableObject
+{
+    public int worldLength = 0;
+    [Range(0, 100)] public float worldPolisDensity = 0f;
+    [Range(0, 100)] public float worldBiomeDensity = 0f;
+}
