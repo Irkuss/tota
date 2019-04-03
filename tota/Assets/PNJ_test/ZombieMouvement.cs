c6bf56189b54da6fc0faf54f1986b7dcc6617531
Jordan FAILLOUX <jordan.failloux@epita.fr>
2019-04-03 23:04:36 +0200

    Test zombie wandering

---
+++ tota/Assets/PNJ_test/ZombieMouvement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieMouvement : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent;
+    public float wanderRadius = 7f;
+    private Vector3 wanderPoint;
+    private NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        wanderPoint = RandomWanderPoint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Wander();
+    }
+
+    void Wander()
+    {
+        if (Vector3.Distance(transform.position, wanderPoint) < 2f)
+        {
+            wanderPoint = RandomWanderPoint();
+        }
+        else
+        {
+            agent.SetDestination(wanderPoint);
+        }
+    }
+
+    Vector3 RandomWanderPoint()
+    {
+        Vector3 randomPoint = (Random.insideUnitSphere * wanderRadius) + transform.position;
+        NavMeshHit Hit;
+        NavMesh.SamplePosition(randomPoint, out Hit, wanderRadius, -1);
+
+        return new Vector3(Hit.position.x, transform.position.y, Hit.position.z);
+    }
+}
