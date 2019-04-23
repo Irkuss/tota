c7b856777a251cdb4c2ec8fafa25a24af1ef1825
potapoe <marcemraiffe@gmail.com>
2019-04-23 17:16:46 +0200

    first

--- tota/Assets/Script/Central/CharaManager.cs
+++ tota/Assets/Script/Central/CharaManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CharaManager : MonoBehaviour
 {
-    private string _charaPath = "CharaTanyako"; //TEMP
+    private string _charaPath = "charamarc"; //TEMP
     //Ref
     [SerializeField] private QuirkTable _quirkTable;
 
